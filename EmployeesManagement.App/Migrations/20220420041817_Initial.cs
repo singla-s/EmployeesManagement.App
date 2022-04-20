370828c2b557f438fe98e17d93ca5af9787fb947
sinnisingla <sinnisingla@outlook.com>
2022-04-20 10:23:14 +0530

    feat: add entity framework

---
+++ EmployeesManagement.App/Migrations/20220420041817_Initial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeesManagement.App.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Salary = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ EmployeesManagement.App/Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeesManagement.App.Models
+{
+    //POCO
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public decimal Salary { get; set; }
+    }
+
+    ////Data Context
+    public class EmployeeDBContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        public EmployeeDBContext(DbContextOptions<EmployeeDBContext> options) : base(options)
+        {
+
+        }
+    }
+}
